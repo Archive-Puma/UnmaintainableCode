73873383e689eae873e748975f41d1fa7f113a0d
Kike Fontan <kikefontanlorenzo@gmail.com>
2018-09-12 12:55:18 +0200

    :small_orange_diamond: PyLint TrueFalse Module

--- modules/c_truefalse.py
+++ modules/c_truefalse.py
@@ -1,13 +1,20 @@
+"""
+ -- UnmaintableCode: C Module --
+ Author: @CosasDePuma <kikefontanlorenzo@gmail.com>(https://github.com/cosasdepuma)
+"""
+# pylint: disable=too-few-public-methods, no-self-use, unused-argument, dangerous-default-value
+
 import re
 
 class Module:
+    """ Redefine True/False Convention """
     def __init__(self, variables):
         pass
 
-    def run_(self, code, args={'n': 3}):
+    def run_(self, code, args={}):
+        """ Define True as 0 and False as 1 """
         code = '#define FALSE 1\n\n' + code
         code = '#define TRUE 0\n' + code
-        
         code = re.sub('\\btrue\\b', 'FALSE', code)
         code = re.sub('\\bfalse\\b', 'TRUE', code)
         return code
