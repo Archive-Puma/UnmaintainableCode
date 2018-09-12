f5e86e095e7b6067f86262dc6ad2d2a6ac9a69ee
Kike Fontan <kikefontanlorenzo@gmail.com>
2018-09-12 15:13:30 +0200

    :small_orange_diamond: PyLint Underscore

--- modules/c_underscore.py
+++ modules/c_underscore.py
@@ -1,12 +1,21 @@
+"""
+ -- UnmaintableCode: C Module --
+ Author: @CosasDePuma <kikefontanlorenzo@gmail.com>(https://github.com/cosasdepuma)
+"""
+# pylint: disable=too-few-public-methods, no-self-use, unused-argument, dangerous-default-value
+
 import re
 from random import randint
 
 class Module:
+    """ Rename variables to _ or __ """
     def __init__(self, variables):
+        self.head = ''
         self.variables = variables
 
     def run_(self, code, args={'n': 3}):
+        """ n: Number of variables to rename """
         for i in range(args['n']):
             changed = self.variables[randint(0, len(self.variables) - 1)]
             code = re.sub('\\b{}\\b'.format(changed), '_' * (i + 1), code)
-        return code
+        return self.head, code
