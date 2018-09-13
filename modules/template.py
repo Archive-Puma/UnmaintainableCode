e924a42ed83ed2d3de8a0908ace0df4fae4f4643
Kike Fontan <kikefontanlorenzo@gmail.com>
2018-09-13 13:12:38 +0200

    :memo: Module Template

---
+++ modules/template.py
@@ -0,0 +1,16 @@
+"""
+ -- UnmaintableCode: Module Template --
+ Author: @CosasDePuma <kikefontanlorenzo@gmail.com>(https://github.com/cosasdepuma)
+"""
+# pylint: disable=too-few-public-methods, no-self-use, unused-argument, dangerous-default-value
+
+class Module:
+    """ Explain the module in a simple way """
+    def __init__(self, variables):
+        self.head = ''
+        # self.variables = variables
+
+    def run_(self, code, args={}):
+        """ Explain the algorithm briefly """
+        # Your code goes here...
+        return self.head, code
